05c60d4d4f4072fbbb45f02c05e68318f70a6656
hanssonsami <13hh1218@amal.se>
2024-09-17 10:33:47 +0200

    Add project files.

---
+++ Roulette/Program.cs
@@ -0,0 +1,13 @@
+﻿int PlaceBet(int currentMoney, string who)
+{
+    int betAmount;
+    while (true)
+    {
+        Console.WriteLine($"{who} har {currentMoney} dollar. Hur mycket vill du satsa?");
+        if (int.TryParse(Console.ReadLine(), out betAmount) && betAmount > 0 && betAmount <= currentMoney)
+        {
+            return betAmount;
+        }
+        Console.WriteLine("Ogiltigt belopp, försök igen.");
+    }
+}
